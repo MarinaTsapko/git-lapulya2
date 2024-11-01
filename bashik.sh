217b9a196afbfb5f9c7edda3aac2a3c36fc94cb6
Live session user <m.e.tsapko@gmail.com>
2024-11-01 20:09:07 +0300

    added bashik.sh

---
+++ bashik.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+get_size() {
+    local path="$1"
+    local size=$(du -hs "$path" | cut -f1)
+    echo $size
+}
+items=$(ls -A)
+
+for item in $items; do
+    size=$(get_size "$item")
+    echo $size"   "$item
+done | sort -nr
